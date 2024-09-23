8ab0b7470de1d54384abbb05cc0371feb8d83e02
skashim <skashim@LGB-5CG424242J>
2024-09-23 16:26:35 +0100

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Shafia",
+                DateOfBirth = new DateTime(1999,04,30)
+            };
+            return View(data);
+        }
+    }
+}
