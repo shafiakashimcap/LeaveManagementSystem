29359540d9be3c6f913dca4a2d195f72b1513823
Shafia <shafia.kashim@capgemini.com>
2024-09-25 15:10:12 +0100

    Completed leave management model

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+
+        public string Name { get; set; } // e.g. sick, vacation, etc
+
+        public int NumberOfdays { get; set; } // e.g. vacation = 30, sick = 10 
+    }
+}
